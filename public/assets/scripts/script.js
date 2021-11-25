51969b8dc9c348d7fa8bbc982b5e8ed4ca35385e
yamanidev <elamine.yamani@gmail.com>
2021-11-25 22:21:01 +0100

    Scroll to top button added

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,8 +1,18 @@
 const burgerToggle = document.getElementById("burgerToggle");
 const topNavLinks = document.getElementsByClassName("top-nav-links")[0];
+const scrollTopBtn = document.getElementById("scroll-top-btn");
 
 
 burgerToggle.addEventListener("click", function () {
     burgerToggle.classList.toggle("active");
     topNavLinks.classList.toggle("active");
-})
+});
+
+window.onscroll = function () {
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+};
+
+scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+});
+
